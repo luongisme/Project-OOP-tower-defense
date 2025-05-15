597a5e5e62624ead72ef5017626515cfd87b549e
Gia Luong <luongduonggia@gmail.com>
2025-05-15 22:57:06 +0700

    delete all unneeded

--- Main/GameStates.java
+++
@@ -1,7 +0,0 @@
-package Main;
-
-public enum GameStates {
-    PLAYING, MENU, SETTINGS;
-
-    public static GameStates gameState = MENU;
-}
--- Scene/SceneMethods.java
+++
@@ -1,7 +0,0 @@
-package Scene;
-
-import java.awt.*;
-
-public interface SceneMethods {
-    public void render(Graphics g);
-}
