06a7fa7fff4670752c37a80e855152eefc1cf3c5
Stardust2321 <minhlenguyenho@gmail.com>
2025-06-07 22:42:53 +0700

    small fix on playin

--- TowerM/TowerManager.java
+++ TowerM/TowerManager.java
@@ -1,41 +1,57 @@
 package TowerM;
-
+import HelperMethod.LoadSave;
 import Scene.Playing;
-
 import java.awt.Graphics;
 import java.awt.image.BufferedImage;
-import HelperMethod.LoadSave;
-import TowerM.GeneralTower;
 import java.util.ArrayList;
+
 
 public class TowerManager {
     private Playing playing;
     private BufferedImage [] towerImages;
-    private GeneralTower tower;
+    private ArrayList<Tower> towers = new ArrayList<>(); // Use ArrayList to manage towers
+    private int towerAmount = 0; // Initialize tower amount
 
     public TowerManager(Playing playing) {
         this.playing = playing;
 
         loadTowerImages();
-        initTower();
     }
 
 
-    private void initTower() {
-      tower = new GeneralTower(0, 0, 0, 0); // Use 0 as default type/index
-    }
+
 
 
     private void loadTowerImages() {
-        BufferedImage atlas=LoadSave.getSpriteAtlas();
+        BufferedImage atlas = LoadSave.getSpriteAtlas();
         towerImages = new BufferedImage[1];
-        towerImages[0]=atlas.getSubimage(64*6, 64, 64, 64); // Use index 0
+        towerImages[0] = atlas.getSubimage(64*6, 64, 64, 64); 
     }
     public void update() {
         
     }
     public void draw(Graphics g) {
-            g.drawImage(towerImages[0], tower.getX(), tower.getY(), null); // Use index 0
+        for (Tower t : towers) {
+            g.drawImage(towerImages[t.getTowerType()], t.getX(), t.getY(), null); 
+        }
+    }
+    
+    public BufferedImage[] getTowerImages(){
+            return towerImages;
+    }
+    
+     public Tower getTowerAt(int x, int y) {
+        for(Tower t : towers) {
+            if(t.getX() == x && t.getY() == y) {
+                return t;
+            }
+        }
+        return null;
+    }
+     
+    public void addTower(Tower selectedTower, int xPos, int yPos) {
+       towers.add(new Tower(xPos, yPos, towerAmount++, selectedTower.getTowerType()));
     }
 
+    
 }
