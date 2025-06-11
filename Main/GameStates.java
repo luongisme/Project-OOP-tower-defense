12c88c1c6737850957c02361b60e7c1bbf2213de
Stardust2321 <minhlenguyenho@gmail.com>
2025-06-11 14:20:04 +0700

    fix game states

--- Main/GameStates.java
+++ Main/GameStates.java
@@ -3,7 +3,7 @@ package Main;
 public enum GameStates {
     PLAYING, MENU, SETTINGS, GAME_OVER;
 
-    public static GameStates gameState = GAME_OVER;
+    public static GameStates gameState = MENU;
 
     public static void SetGameState(GameStates state){
         gameState = state;
